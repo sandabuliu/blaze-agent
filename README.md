20e10c8fb822d5cddced9aba8793dc1eb2a68a47
tongbin <tongbin@haizhi.com>
2017-04-28 16:53:34 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# The plugin of agent for blaze
+
+
+#### install:
+
+```shell
+pip install git+https://github.com/sandabuliu/blaze-agent.git
+```
+or      
+```shell
+git clone https://github.com/sandabuliu/blaze-agent.git
+cd python-agent
+python setup.py install
+```
+
+#### example:
+
+```python
+from agent import rule
+from blaze import data, compute, by
+
+df = data('agent:/var/log/nginx/access.log', rule=rule('nginx'))
+print df.peek()     #  预览数据
+print compute(df.count(), chunks=2, chunksize=2000)  #  计算两个窗口内的个数, 每个窗口大小为 2000
+print compute(df.method.distinct().count(), chunks=1, chunksize=10000)  #  计算一个窗口内的请求方法种类, 每个窗口大小为10000
+print compute(df.body_bytes_sent.max())  #  计算最大发送包大小
+print compute(by(df.remote_addr, count=df.count()))  #  统计不同IP的访问量 
+```
---
+++ blaze_agent/__init__.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from blaze.utils import available_memory
+from blaze.dispatch import dispatch
+from blaze.expr.collections import Expr
+from blaze import resource, discover, convert, DataFrame, odo, chunks
+
+try:
+    from agent import source, Agent
+except ImportError:
+    raise Exception('Please install python-agent by '
+                    'pip install git+https://github.com/sandabuliu/python-agent.git')
+
+
+__author__ = 'tong'
+__version__ = '1.0.0'
+
+
+DEFAULT_CHUNKSIZE = 1000
+
+
+class AGENT(object):
+    def __init__(self, uri, **kwargs):
+        self.uri = uri
+        self.kwargs = kwargs
+
+    @property
+    def path(self):
+        return self.uri[6:]
+
+    def agent(self, chunkno=0, chunksize=DEFAULT_CHUNKSIZE):
+        if chunkno > 0:
+            startline = chunksize * chunkno
+        elif chunkno < 0:
+            raise Exception('Not Implement')
+        else:
+            startline = 0
+        return Agent(source.File(self.uri[6:], startline=startline), rule=self.kwargs.get('rule'))
+
+
+@convert.register(DataFrame, AGENT)
+def convert_agent(agt, **kwargs):
+    return DataFrame([i for i in agt.agent()])
+
+
+@convert.register(chunks(DataFrame), AGENT)
+def convert_agent(agt, **kwargs):
+    def _(ag):
+        chunks = kwargs.get('chunks', None)
+        ret = []
+        for i, item in enumerate(ag, 1):
+            if chunks == 0:
+                return
+            ret.append(item)
+            if i % kwargs.get('chunksize', DEFAULT_CHUNKSIZE) == 0:
+                yield DataFrame(ret)
+                if chunks:
+                    chunks -= 1
+                ret = []
+        yield DataFrame(ret)
+    chunkno = kwargs.get('chunkno', 0)
+    chunksize = kwargs.get('chunksize', 0)
+    return chunks(DataFrame)(_(agt.agent(chunkno, chunksize)))
+
+
+@discover.register(AGENT)
+def discover_deque(agt, **kwargs):
+    for i in agt.agent():
+        return discover([i])
+
+
+@dispatch(Expr, AGENT)
+def pre_compute(expr, data, **kwargs):
+    comfortable_memory = min(1e9, available_memory()/4)
+
+    if os.path.getsize(data.path) > comfortable_memory:
+        kwargs['chunksize'] = kwargs.get('chunksize') or DEFAULT_CHUNKSIZE
+
+    leaf = expr._leaves()[0]
+    if kwargs.get('chunksize'):
+        return odo(data, chunks(DataFrame), dshape=leaf.dshape, **kwargs)
+    else:
+        return odo(data, DataFrame, dshape=leaf.dshape, **kwargs)
+
+
+@resource.register('agent:.+')
+def resource_agent(uri, **kwargs):
+    return AGENT(uri, **kwargs)
+
